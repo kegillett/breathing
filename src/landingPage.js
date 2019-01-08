c40498a1fe4a3610dc4ac21b3a44ff7b6ecc3eb6
kegillett <kylegillett@gmail.com>
2019-01-08 14:48:33 -0700

    first commit

---
+++ src/landingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>I am a landing Page</p>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
